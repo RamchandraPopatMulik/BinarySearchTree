e78c859f0f45bf914402e72149f63d8ee9675b80
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-20 16:20:17 +0530

    Added file

--- Binary_Search_Tree/Node.cs
+++ Binary_Search_Tree/Node.cs
@@ -3,67 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Binary_Search_Tree
 {
-    public class BinarySearchTree<T> where T : IComparable<T>
+    internal class Node
     {
-        public T NodeData { get; set; }
-        public BinarySearchTree<T> LeftTree { get; set; }
-        public BinarySearchTree<T> RightTree { get; set; }
-
-        public BinarySearchTree(T nodeData)
-        {
-            this.NodeData = nodeData;
-            this.LeftTree = null;
-            this.RightTree = null;
-        }
-        int leftCount = 0;
-        int rightCount = 0;
-        bool result = false;
-        public void Add(T items)
-        {
-            T currentNodeValue = this.NodeData;
-            if ((currentNodeValue.CompareTo(items)) > 0)
-            {
-                if (this.LeftTree == null)
-                {
-                    this.LeftTree = new BinarySearchTree<T>(items); ;
-                }
-                else
-                {
-                    this.LeftTree.Add(items);
-                    return;
-                }
-            }
-            else
-            {
-                if (this.RightTree == null)
-                {
-                    this.RightTree = new BinarySearchTree<T>(items);
-                }
-                else
-                {
-                    this.RightTree.Add(items);
-                    return;
-                }
-            }
-            Console.WriteLine($"Added : {items}");
-        }
-        public void Display()
-        {
-            if (this.LeftTree != null)
-            {
-                this.leftCount++;
-                this.LeftTree.Display();
-            }
-            Console.WriteLine(this.NodeData.ToString());
-            if (this.RightTree != null)
-            {
-                this.rightCount++;
-                this.RightTree.Display();
-            }
-        }
     }
 }
