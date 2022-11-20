09159de22b43f6e3b7a619d9494a1053908ad355
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-20 16:10:32 +0530

    Added BinarySearchTree

---
+++ Binary_Search_Tree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Binary_Search_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(56);
+            tree.Add(30);
+            tree.Add(70);
+
+            Console.WriteLine("********************** Elements in Binary Trees **********************");
+            tree.Display(); 
+        }
+    }
+}
